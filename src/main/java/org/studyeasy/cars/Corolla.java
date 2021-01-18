1de8eeae37f2273efac0cd1b6113af8ddcdf2504
edgardoferreyra <edgardo.ivan.ferreyra@gmail.com>
2021-01-18 19:56:43 -0300

    first commit.

---
+++ src/main/java/org/studyeasy/cars/Corolla.java
@@ -0,0 +1,36 @@
+package org.studyeasy.cars;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.studyeasy.interfaces.Car;
+import org.studyeasy.interfaces.Engine;
+
+@Component("corolla")
+public class Corolla implements Car {
+	
+	@Autowired
+	@Qualifier("V8Engine")
+	Engine engine;
+	
+	@Override
+	public String specs() {
+		
+		String specs = "Sedan from Toyota with engine type "+ engine.type();
+		return specs;
+	}
+/*	
+	public Corolla(Engine engine) {
+		engine.type = "New V8 engine";
+		this.engine = engine;
+	}
+*/	
+/*
+	@Autowired
+	public void setEngine(Engine engine) {
+		engine.type = "New V8 engine";
+		this.engine = engine;
+	}
+	*/
+}
+ 
